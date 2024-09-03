08542e3f83d00bedd71fe1c49122955db32b0a34
FanLubLou <jaclermont@hotmail.com>
2024-09-03 15:24:29 +0200

    implementation des commentaires JSDocs pour le fichier config js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,24 @@
-export const USE_MOCK_DATA = false; // Passer à `false` pour utiliser les données du backend
+/**
+ * @file config.js
+ * @description Fichier de configuration pour gérer la source des données dans l'application.
+ * @module config
+ */
+
+/**
+ * Indique si les données mockées doivent être utilisées plutôt que les données du backend.
+ * 
+ * Lorsque `USE_MOCK_DATA` est défini sur `true`, l'application utilise les données mockées pour les 
+ * utilisateurs et les autres données. Lorsque `USE_MOCK_DATA` est défini sur `false`, l'application
+ * récupère les données directement depuis le backend via les services API.
+ * 
+ * @type {boolean}
+ * @default false
+ * @example
+ * // Utiliser les données du backend
+ * export const USE_MOCK_DATA = false;
+ * 
+ * // Utiliser les données mockées
+ * export const USE_MOCK_DATA = true;
+ */
+
+export const USE_MOCK_DATA = false; 
